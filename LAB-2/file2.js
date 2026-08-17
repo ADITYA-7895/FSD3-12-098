adce77240ceb7521dd8f406311fa2cc5b6e7d243
Aditya Saxena <adityasaxenaixcs.12.194@gmail.com>
2026-08-17 11:50:40 +0530

    js

---
+++ LAB-2/file2.js
@@ -0,0 +1,12 @@
+import { readfile } from "fs/promises";
+
+const readData = (filename)=>{
+    const content = await readFile(filename,'utf-8');
+    return content;
+}
+const writeData = (filename,content)=>{
+    await writeFile{filename,content};
+
+}
+const appendData = (filename,content)=>{
+    await appendfile{filename,content};
